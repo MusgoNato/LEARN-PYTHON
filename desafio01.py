2a07760208f48958a8bf14805b8b4a73180e6b18
MusgoNato <hugojosue03@gmail.com>
2023-12-08 18:05:42 -0400

    desafio 1 e 2
    Tenho que lembrar que o '+' ele vai juntar as strings, o ',' ele junta os tipos de variaveis, independente de qual for ela.

--- desafio01.py
+++ desafio01.py
@@ -1,3 +1,11 @@
 #O desafio é imprimir na tela as boas vindas ao usuario de acordo com o valor digitado
-nome = input('Qual o seu nome ')
-print('Boas vindas', nome , 'prazer em conhecê-lo')
+#Todos os arquivos nessa pasta sao referenetes ao 1° modulo do python no curso em video
+#Esse é o primeiro video, primeiro desafio
+'''nome = input('Qual o seu nome ')
+print('Boas vindas', nome , 'prazer em conhecê-lo')'''
+
+#Segundo desafio
+dia = input('Dia: ')
+mes = input('Mes: ')
+ano = input('Ano: ')
+print('Voce nasceu em', dia, 'de', mes, 'do ano de' , ano)# O + vai concatenar as stirngs, o virgula é um tipo de separador, independente de qual tipo for a variavel
