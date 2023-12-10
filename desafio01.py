37d9a0e6045261f2994403137ae72e44764aee10
MusgoNato <hugojosue03@gmail.com>
2023-12-10 19:14:05 -0400

    Modificações em alguns comentários e exercícios
    Modifiquei alguns comentários dos desafios pra eu saber qual era o enunciado; Alguns desafios eram simples entao decidi melhorar eles

--- desafio01.py
+++ desafio01.py
@@ -1,6 +1,6 @@
 #O desafio é imprimir na tela as boas vindas ao usuario de acordo com o valor digitado
 #Todos os arquivos nessa pasta sao referenetes ao 1° modulo do python no curso em video
-#Esse é o primeiro video, primeiro desafio
+#Esse é o primeiro video, primeiro desafio (Boas vindas ao usuário)
 '''nome = input('Qual o seu nome ')
 print('Boas vindas', nome , 'prazer em conhecê-lo')'''
 
@@ -10,7 +10,7 @@ mes = input('Mes: ')
 ano = input('Ano: ')
 print('Voce nasceu em', dia, 'de', mes, 'do ano de' , ano)# O + vai concatenar as stirngs, o virgula é um tipo de separador, independente de qual tipo for a variavel
 #Um outro jeito de imprimir na tela a mesma coisa porem utilizando '{}' e '.format()', segue abaixo
-print('Voce nasceu em {} de {} do ano de {}' .format(dia, mes, ano)) #O format vai seguir a ordem das chaves para impressão'''
+print('Voce nasceu em {} de {} do ano de {}' .format(dia, mes, ano)) #O format vai seguir a ordem das chaves para impressão, o chaves é chamado de mascara'''
 
 #Terceiro desafio (Soma de 2 dois numeros)
 #aqui ele fala sobre os tipos primitivos, antes de pedir o numero ao usuario ele formata a string para um tipo int, para que depois seja somado e nao concatenado
@@ -19,12 +19,12 @@ num2 = int(input('Insira um numero: '))
 soma = num1 + num2
 print('A soma entre {} e {} vale {}'.format(num1, num2, soma))'''
 
-#No caso do valor booleano, ele so retorna verdadeiro se tem algo dentro da variavel, caso nao tenha retorna False
+#No caso do valor booleano, ele so retorna verdadeiro se tem algo dentro da variavel, caso nao tenha retorna False (Exemplo de false é apertando 'enter')
 '''n1 = bool(input('Insira algo: '))
 print(n1)'''
 
-#Existe um modulo que é o is.numeric('Variavel aqui dentro'), ele vai dizer se é possivel converter o valor da variavel em um numero retornando True ou False, o mesmo vale para os outros tipos depois do 'is'
-#Quarto desafio
+#Existe um modulo que é o 'is'algumacoisa(), ele vai dizer se é possivel converter o valor da variavel em um numero retornando True ou False, o mesmo vale para os outros tipos depois do 'is'
+#Quarto desafio (Todas as informações de uma variável)
 '''variavel = input('Insira algo: ')
 print(variavel.isalnum()) #Me diz se a variavel é numero
 print(variavel.isalpha()) #Se é do alfabeto
@@ -45,26 +45,26 @@ print(variavel.isupper()) #Verifica se a string esta com os caracteres todos mai
 '''Operadores aitmeticos '+' -> Soma   '*' -> Multiplicação '/' -> Divisão '-' -> Subtração '**' -> Potência '//' -> Divisão inteira '%' -> Resto da divisão '''
 #Qualquer numero elevado a (1/2) vai dar sua raiz
 
-#Quinto desafio
+#Quinto desafio (Sucessor e antecessor de um numero)
 '''num = int(input('Digite um numero: '))
 print('O antecessor de {} é {} e o sucessor é {}' .format(num, num - 1, num + 1)) #O format aceita fazer aritmetica simples pra imprimir depois'''
 
-#Sexto desafio
+#Sexto desafio (Dobro, triplo e raiz quadrada de um numero)
 '''num = int(input('Insira um numero: '))
-print('O dobro de {} é {}, o triplo é {} e a raiz quadrada é {:.2f}' .format(num, num * 2, num * 3, num ** (1/2))) #O ':.2f' dentro do {}, é pra deixar com dois numeros apos o ponto'''
+print('O dobro de {} é {}, o triplo é {} e a raiz quadrada é {:.2f}' .format(num, num * 2, num * 3, num ** (1/2))) #O ':.2f' dentro do {}, é pra deixar com dois numeros apos o ponto flutuante'''
 
-#Setimo desafio
+#Setimo desafio (Media de duas notas de um aluno)
 '''nome = str(input('Qual o nome do aluno: '))
 nota1 = int(input('Insira a nota 1: '))
 nota2 = int(input('Insira a nota 2: '))
 media = (nota1 + nota2)/2
 print('Media do aluno {} é {:.2f}' .format(nome, float(media)))'''
 
-#Oitavo desafio
+#Oitavo desafio (Conversão de metros para centimetros e milimetros)
 '''metros = int(input('Insira a medida em metros: '))
 print('A medida de {} metros em centimetros é {}, em milimetros é {}'.format(metros, metros * 10, metros * 100))'''
 
-#Nono desafio
+#Nono desafio (Tabuada de um numero qualquer)
 #Versao podre
 '''num = int(input('Insira um numero: '))
 print('\nTABUADA\n')
@@ -83,18 +83,18 @@ print('10 X {} = {}\n' .format(num, num * 10))'''
 '''num = int((input('Insira um numero: ')))
 print('\nTABUADA\n')
 for i in range(1, num + 1, 1): # Aqui o for funciona criando uma variavel a qual é o 'i', e vai ate o range('inicio, parada, incremento')
-    print('{} X {} = {}' .format(num, i, num * i)) #Aqui pensei em simplificar, multipliquei as variaveis ao inves de criar uma variavel pra armaenar a multiplicação do 'i' e o 'num'
+    print('{} X {} = {}' .format(num, i, num * i)) #Aqui pensei em simplificar, multipliquei as variaveis ao inves de criar uma variavel pra armazenar a multiplicação do 'i' e o 'num'
 print('\n')'''
 
 #10° desafio (Quanto tem de dinheiro na carteira do usuario e quantos dolares ele consegue comprar com o dinheiro dele)
 '''num = float(input('Quanto dinheiro há na sua carteira? '))
-print('Quantidade na carteira {:.2f}, dolares que podem ser comprados {:.2f}' .format(num, num / 4,93))'''
+print('Quantidade na carteira {:.2f}, dolares que podem ser comprados {:.2f}' .format(num, num / 4,93)) #Cotação 'atual' do dolar'''
 
 #11° desafio (Ler largura e altura de uma parede em metros e calcular a area , cada litro de tinta é 2m² por area, quantos preciso pra pintar a parede toda?)
 '''largura = float(input('Insira a largura da parede: '))
 altura = float(input('Insira a altura da parede: '))
 area = largura * altura
-print('Voce precisa de {:.2f} litros para pintar sua parede!'.format(area * pow(2, 2)))'''
+print('Voce precisa de {:.2f} litros para pintar sua parede!'.format(area/2))'''
 
 #12° desafio (Ler um preco de um produto, devolve-lo com 5% de desconto)
 '''produto = float(input('Qual o valor do produto? '))
@@ -103,7 +103,8 @@ produto = produto - (produto * (desconto/100))
 print('Valor final com 5% de desconto é {}!' .format(produto))'''
 
 #13° desafio (Ler um salario de um funcionário e devolve com 15% de aumento)
-'''nome = str(input('Qual o nome do funcionário? '))
+nome = str(input('Qual o nome do funcionário? '))
+aumento = int(input('Quantos de aumento {} receberá? '.format(nome)))
 salario = float(input('Qual o sálario do {}? '. format(nome))) #Da pra aplicar o format direto no input
-aumento = salario + (salario * (15/100)) #burrooooo, era 15/100, quase 30m tentando resolver e era só inverter a ordem
-print('O sálario do funcionário {} é {:.2f}, com aumento fica {:.2f}!' .format(nome, salario, aumento))'''
+aumento = salario + (salario * (aumento/100)) #burrooooo, era aumento/100, quase 30m tentando resolver e era só inverter a ordem
+print('O sálario do funcionário {} é {:.2f}, com aumento fica {:.2f}!' .format(nome, salario, aumento))
